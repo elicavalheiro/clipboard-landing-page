ff323d3fc1a21c205b32e0db39714134c0e15fc9
elicavalheiro <elimara.ac@gmail.com>
2021-08-26 23:10:04 -0300

    :lipstick: adds button styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './assets/style/style.scss';
+import { Button } from './components/Button/Button';
 
 function App() {
-  return <div className="App">Clipboard Landing Page</div>;
+  return (
+    <div className="App">
+      Clipboard Landing Page
+      <Button variation="primary">Download for iOS</Button>
+      <Button variation="secondary">Download for Mac</Button>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ButtonProps, ButtonVariationsType } from './Button.types';
 
+import './Button.style.scss'
+
 export const Button = ({ children, variation }: ButtonProps) => {
   const variations: ButtonVariationsType = {
-    primary: 'button__primary-green',
-    secondary: 'button__secondary-blue',
+    primary: 'button button__primary-green',
+    secondary: 'button button__secondary-blue',
   };
 
   return <button className={variations[variation]}>{children}</button>;
